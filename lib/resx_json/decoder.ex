b0af2c235f3c1c3348158b771af084a7c36821ce
Stefan Johnson <sj@scrimpycat.io>
2019-02-16 01:54:25 +1100

    Support path expression queries

--- lib/resx_json/decoder.ex
+++ lib/resx_json/decoder.ex
@@ -5,7 +5,15 @@ defmodule ResxJSON.Decoder do
 
     @impl Resx.Transformer
     def transform(resource = %{ content: content }, opts) do
-        content = Content.Stream.new(content)
-        { :ok, %{ resource | content: %{ content | data: Content.Stream.new(content) |> Jaxon.Stream.query(opts[:query] || [:root]) } } }
+        case format_query(opts[:query]) do
+            { :ok, query } ->
+                content = Content.Stream.new(content)
+                { :ok, %{ resource | content: %{ content | data: Content.Stream.new(content) |> Jaxon.Stream.query(query) } } }
+            { :error, error } -> { :error, { :internal, "Invalid query format: " <> error.message } }
+        end
     end
+
+    defp format_query(nil), do: { :ok, [:root] }
+    defp format_query(query) when is_list(query), do: { :ok, query }
+    defp format_query(query), do: Jaxon.Path.parse(query)
 end
