6453bf9c57808dce2e7056dbcadce7904fa1e500
Stefan Johnson <sj@scrimpycat.io>
2019-02-16 01:12:21 +1100

    Test queries

---
+++ test/resx_json/decoder_test.exs
@@ -0,0 +1,25 @@
+defmodule ResxJSON.DecoderTest do
+    use ExUnit.Case
+
+    test "queries" do
+        assert [%{}] == (Resx.Resource.open!(~S(data:application/json,{})) |> Resx.Resource.transform!(ResxJSON.Decoder)).content |> Resx.Resource.Content.data
+        assert [%{ "a" => "foo", "b" => "bar" }] == (Resx.Resource.open!(~S(data:application/json,{"a": "foo", "b": "bar"})) |> Resx.Resource.transform!(ResxJSON.Decoder)).content |> Resx.Resource.Content.data
+        assert [%{ "a" => 1, "b" => 2 }] == (Resx.Resource.open!(~S(data:application/json,{"a": 1, "b": 2})) |> Resx.Resource.transform!(ResxJSON.Decoder)).content |> Resx.Resource.Content.data
+        assert [%{ "a" => [1, 2, 3], "b" => [4, 5, 6] }] == (Resx.Resource.open!(~S(data:application/json,{"a": [1, 2, 3], "b": [4, 5, 6]})) |> Resx.Resource.transform!(ResxJSON.Decoder)).content |> Resx.Resource.Content.data
+        assert [%{ "a" => %{ "x" => 1, "y" => 2 }, "b" => %{ "x" => 3, "y" => 4 } }] == (Resx.Resource.open!(~S(data:application/json,{"a": {"x": 1, "y": 2}, "b": {"x": 3, "y": 4}})) |> Resx.Resource.transform!(ResxJSON.Decoder)).content |> Resx.Resource.Content.data
+
+        assert [%{}] == (Resx.Resource.open!(~S(data:application/json,{})) |> Resx.Resource.transform!(ResxJSON.Decoder, query: [:root])).content |> Resx.Resource.Content.data
+        assert [%{ "a" => "foo", "b" => "bar" }] == (Resx.Resource.open!(~S(data:application/json,{"a": "foo", "b": "bar"})) |> Resx.Resource.transform!(ResxJSON.Decoder, query: [:root])).content |> Resx.Resource.Content.data
+        assert [%{ "a" => 1, "b" => 2 }] == (Resx.Resource.open!(~S(data:application/json,{"a": 1, "b": 2})) |> Resx.Resource.transform!(ResxJSON.Decoder, query: [:root])).content |> Resx.Resource.Content.data
+        assert [%{ "a" => [1, 2, 3], "b" => [4, 5, 6] }] == (Resx.Resource.open!(~S(data:application/json,{"a": [1, 2, 3], "b": [4, 5, 6]})) |> Resx.Resource.transform!(ResxJSON.Decoder, query: [:root])).content |> Resx.Resource.Content.data
+        assert [%{ "a" => %{ "x" => 1, "y" => 2 }, "b" => %{ "x" => 3, "y" => 4 } }] == (Resx.Resource.open!(~S(data:application/json,{"a": {"x": 1, "y": 2}, "b": {"x": 3, "y": 4}})) |> Resx.Resource.transform!(ResxJSON.Decoder, query: [:root])).content |> Resx.Resource.Content.data
+
+        assert [1, 2, 3] == (Resx.Resource.open!(~S(data:application/json,{"a": [1, 2, 3], "b": [4, 5, 6]})) |> Resx.Resource.transform!(ResxJSON.Decoder, query: [:root, "a", :all])).content |> Resx.Resource.Content.data
+        assert [4, 5, 6] == (Resx.Resource.open!(~S(data:application/json,{"a": [1, 2, 3], "b": [4, 5, 6]})) |> Resx.Resource.transform!(ResxJSON.Decoder, query: [:root, "b", :all])).content |> Resx.Resource.Content.data
+        assert [4] == (Resx.Resource.open!(~S(data:application/json,{"a": [1, 2, 3], "b": [4, 5, 6]})) |> Resx.Resource.transform!(ResxJSON.Decoder, query: [:root, "b", 0])).content |> Resx.Resource.Content.data
+        assert [%{ "x" => 1, "y" => 2 }] == (Resx.Resource.open!(~S(data:application/json,{"a": {"x": 1, "y": 2}, "b": {"x": 3, "y": 4}})) |> Resx.Resource.transform!(ResxJSON.Decoder, query: [:root, "a"])).content |> Resx.Resource.Content.data
+        assert [1] == (Resx.Resource.open!(~S(data:application/json,{"a": {"x": 1, "y": 2}, "b": {"x": 3, "y": 4}})) |> Resx.Resource.transform!(ResxJSON.Decoder, query: [:root, "a", "x"])).content |> Resx.Resource.Content.data
+        assert [1, 2, "foo"] == (Resx.Resource.open!(~S(data:application/json,[{"a": 1}, {"a": 2}, {"a": "foo"}])) |> Resx.Resource.transform!(ResxJSON.Decoder, query: [:all, "a"])).content |> Resx.Resource.Content.data
+        assert [2] == (Resx.Resource.open!(~S(data:application/json,[{"a": 1}, {"a": 2}, {"a": "foo"}])) |> Resx.Resource.transform!(ResxJSON.Decoder, query: [1, "a"])).content |> Resx.Resource.Content.data
+    end
+end
