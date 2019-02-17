d9ae41eec2ff21a21ab4617316b1bb24c00a658d
Stefan Johnson <sj@scrimpycat.io>
2019-02-17 15:31:57 +1100

    Ensure resource type is json and replace with x.erlang.native

--- lib/resx_json/decoder.ex
+++ lib/resx_json/decoder.ex
@@ -1,5 +1,5 @@
 defmodule ResxJSON.Decoder do
-    @behaviour Resx.Transformer
+    use Resx.Transformer
 
     alias Resx.Resource.Content
 
@@ -7,8 +7,12 @@ defmodule ResxJSON.Decoder do
     def transform(resource = %{ content: content }, opts) do
         case format_query(opts[:query]) do
             { :ok, query } ->
-                content = Content.Stream.new(content)
-                { :ok, %{ resource | content: %{ content | data: Content.Stream.new(content) |> Jaxon.Stream.query(query) } } }
+                case validate_type(content.type) do
+                    { :ok, type } ->
+                        content = Content.Stream.new(content)
+                        { :ok, %{ resource | content: %{ content | type: type, data: Content.Stream.new(content) |> Jaxon.Stream.query(query) } } }
+                    error -> error
+                end
             { :error, error } -> { :error, { :internal, "Invalid query format: " <> error.message } }
         end
     end
@@ -16,4 +20,13 @@ defmodule ResxJSON.Decoder do
     defp format_query(nil), do: { :ok, [:root] }
     defp format_query(query) when is_list(query), do: { :ok, query }
     defp format_query(query), do: Jaxon.Path.parse(query)
+
+    defp validate_type([type|types]) do
+        match = ~r/\/(json(\+json)?|(.*?\+)json)(;|$)/
+        if type =~ match do
+            { :ok, [String.replace(type, match, "/\\3x.erlang.native\\4")|types] }
+        else
+            { :error, { :internal, "Invalid resource type" } }
+        end
+    end
 end
