cb37a13cc73a48fdf6cba9cea7149f5f8d7ad715
Stefan Johnson <sj@scrimpycat.io>
2019-02-20 06:26:02 +1100

    Prepare for release

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # ResxJSON
-A JSON encoding/decoding transformer for resx
+A JSON encoding/decoding transformer for [resx](https://github.com/ScrimpyCat/Resx)
+
+Installation
+------------
+
+```elixir
+def deps do
+    [{ :resx_json, "~> 0.1.0" }]
+end
+```
--- mix.exs
+++ mix.exs
@@ -5,11 +5,12 @@ defmodule ResxJSON.MixProject do
         [
             app: :resx_json,
             description: "JSON encoding/decoding transformer for the resx library",
-            version: "0.0.3",
+            version: "0.1.0",
             elixir: "~> 1.7",
             start_permanent: Mix.env() == :prod,
             deps: deps(),
-            dialyzer: [plt_add_deps: :transitive]
+            dialyzer: [plt_add_deps: :transitive],
+            package: package()
         ]
     end
 
@@ -24,6 +25,14 @@ defmodule ResxJSON.MixProject do
             { :ex_doc, "~> 0.18", only: :dev, runtime: false },
             { :simple_markdown, "~> 0.5.4", only: :dev, runtime: false },
             { :ex_doc_simple_markdown, "~> 0.3", only: :dev, runtime: false }
+        ]
+    end
+
+    defp package do
+        [
+            maintainers: ["Stefan Johnson"],
+            licenses: ["BSD 2-Clause"],
+            links: %{ "GitHub" => "https://github.com/ScrimpyCat/ResxJSON" }
         ]
     end
 end
