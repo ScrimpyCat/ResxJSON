a60b7be757d772e87d2129e6ca8e21f793cafe11
Stefan Johnson <sj@scrimpycat.io>
2019-02-26 13:07:47 +1100

    Document partial sequence

--- lib/resx_json/partial/sequence.ex
+++ lib/resx_json/partial/sequence.ex
@@ -1,3 +1,15 @@
 defmodule ResxJSON.Partial.Sequence do
+    @moduledoc """
+      This struct can be used to group a batch of partials in a partial stream
+      that will be processed by `ResxJSON.Encoder`.
+
+        %ResxJSON.Partial.Sequence{
+            nodes: [
+                %ResxJSON.Partial.key("foo"),
+                "bar",
+            ]
+        }
+    """
+
     defstruct [nodes: []]
 end
